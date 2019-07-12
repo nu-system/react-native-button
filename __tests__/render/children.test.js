33916a99b415b466156cb2391a82eff7cfbd6dea
nanaSun <2694748344@qq.com>
2019-07-12 20:42:32 +0800

    test render+ readme

---
+++ __tests__/render/children.test.js
@@ -0,0 +1,24 @@
+import 'react-native';
+import React from 'react';
+import NuButton from '../../lib/index';
+import {shallow} from 'enzyme';
+
+describe("children属性",()=>{
+    //'default', 'primary', 'secondary', 'warning', 'danger', 'success'
+    it(`纯文字`,()=>{
+        const tmp= shallow(<NuButton>default</NuButton>).find("Text")
+        const {theme , style, Pre, Append, ...userProps}=tmp.props()
+        expect(tmp.prop("children")).toBe("default")
+    })
+    it(`文字+空格+文字`,()=>{
+        const tmp= shallow(<NuButton>default red</NuButton>).find("Text")
+        const {theme , style, Pre, Append, ...userProps}=tmp.props()
+        expect(tmp.prop("children")).toBe("default red")
+    })
+    it(`文字+变量`,()=>{
+        const red="red";
+        const tmp= shallow(<NuButton>default {red}</NuButton>).find("Text")
+        const {theme , style, Pre, Append, ...userProps}=tmp.props()
+        expect(tmp.prop("children")).toBe("default red")
+    })
+})
